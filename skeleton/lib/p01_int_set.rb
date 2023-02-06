cd8b073f72954e738779aad264ce5c68864fa203
Dan Holodak <danholodak@gmail.com>
2023-02-06 14:22:23 -0500

    Add IntSet class

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -39,23 +39,37 @@ class MaxIntSet
 end
 
 class IntSet
+
+  attr_reader :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    if !include?(num)
+      self[num] << num
+    end
+
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+    end
+
   end
 
   def include?(num)
+    self[num].include?(num)
+
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    i = num % num_buckets
+    return store[i]
   end
 
   def num_buckets
